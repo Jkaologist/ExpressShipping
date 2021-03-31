611dc5c5bc55c8e22312b42e3c34192a6091a779
David Zemel <zemel96@gmail.com>
2021-03-30 20:45:55 -0400

    part 2 complete

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,21 @@
 "use strict";
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
 
-const {
-  shipProduct,
-} = require("./shipItApi");
-
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
 
 test("shipProduct", async function () {
+  axiosMock.onPost(SHIPIT_SHIP_URL).reply(200, {
+    receipt: {
+      itemId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+      shipId: 1,
+    },
+  });
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
